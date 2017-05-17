5cb4d2593ae1370ba46e2d4f2a281db6b1f59087
zhegu707 <affe@live.cn>
2017-05-17 23:17:37 +0800

    4
    4

--- application/controllers/mobilityAnalysis/RegionPostChange.php
+++ application/controllers/mobilityAnalysis/RegionPostChange.php
@@ -8,9 +8,43 @@ class RegionPostChange extends CI_Controller {
         date_default_timezone_set('UTC');
         $time = date("Y-m-d H:i:s");
         $result = array("code"=>200, "status"=>"success", "message"=>"success","time"=>$time);
-        $data = array(array(
-            "index"=>"78"
-        )
+        $data = array(
+            array(
+            "region"=>"朝阳区",
+            "regionNum"=>"100",
+            "postYesNum"=>"58",
+            "proportion"=>"58%"   
+            ),
+            array(
+                "region"=>"朝阳区",
+                "regionNum"=>"100",
+                "postYesNum"=>"58",
+                "proportion"=>"58%"
+            ),
+            array(
+                "region"=>"东城区",
+                "regionNum"=>"100",
+                "postYesNum"=>"58",
+                "proportion"=>"58%"
+            ),
+            array(
+                "region"=>"西城区",
+                "regionNum"=>"100",
+                "postYesNum"=>"58",
+                "proportion"=>"58%"
+            ),
+            array(
+                "region"=>"通州区",
+                "regionNum"=>"100",
+                "postYesNum"=>"58",
+                "proportion"=>"58%"
+            ),
+            array(
+                "region"=>"顺义区",
+                "regionNum"=>"100",
+                "postYesNum"=>"58",
+                "proportion"=>"58%"
+            )
         );
         $result["data"] = $data;
         echo json_encode($result);
