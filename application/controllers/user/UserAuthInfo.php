a59637ae393c4d541341f7f4f80d54474878da0a
zhegu707 <affe@live.cn>
2017-05-18 11:08:25 +0800

    权限数据
    权限数据

--- application/controllers/user/UserAuthInfo.php
+++ application/controllers/user/UserAuthInfo.php
@@ -9,15 +9,12 @@ class UserAuthInfo extends CI_Controller {
         $result = array("code"=>200, "status"=>"success", "message"=>"success","time"=>$time);
         $data = array(
             array(
-                "userName"=>"张三",
-                "userAccountNum"=>"18500000000",
-                "province"=>"江苏省",
-                "city"=>"连云港市",
-                "district"=>"",
-                "street"=>"张三",
-                "grid"=>"张三",
-                "roleName"=>"地市",
-                "postName"=>"总经理"
+                "regionalScope"=>array(
+                    array("region1"=>"连云港市","region2"=>"海州区","region3"=>"海州街道、幸福路街道、朐阳街道、洪门街道、新坝镇、锦屏镇、板浦镇、宁海街道、浦东街道、浦西街道、新东街道、新南街道、路南街道、新海街道、花果山街道、南城街道、云台街道、浦南镇、云台农场、南云台林场、岗埠农场"),
+                    array("region1"=>"连云港市","region2"=>"连云区","region3"=>"墟沟街道、海州湾街道、连云街道、连岛街道、板桥街道、徐圩街道、朝阳街道、云山街道、宿城街道、高公岛街道、中云街道、猴嘴街道、东辛农场"),
+                    array("region1"=>"连云港市","region2"=>"赣榆区","region3"=>"青口镇、柘汪镇、石桥镇、金山镇、黑林镇、厉庄镇、海头镇、塔山镇、赣马镇、班庄镇、城头镇、城西镇、宋庄镇、沙河镇、墩尚镇")
+                ),
+                "postList"=>array("渠道人员","装维人员","全部人员"),
             )
         );
         $result["data"] = $data;
