52710d073b0fe87c97c68318e63ee709149e0d15
zhegu707 <affe@live.cn>
2017-05-18 21:38:18 +0800

    登陆添加token

--- application/controllers/user/UserLogin.php
+++ application/controllers/user/UserLogin.php
@@ -6,7 +6,7 @@ class UserLogin extends CI_Controller {
     {
         date_default_timezone_set('UTC');
         $time = date("Y-m-d H:i:s");
-        $result = array("code"=>200, "status"=>"success", "message"=>"success","time"=>$time);
+        $result = array("code"=>200, "status"=>"success", "message"=>"success","time"=>$time,"validDuration"=>180,"authToken"=>"4cdbc040-657a-4847-b266-7e31d9e2c3d9","refreshToken"=>"7cdbc040-657a-4847-b266-7e31d9e2c300");
         $data = array(
             array(
                 "userName"=>"张三",
